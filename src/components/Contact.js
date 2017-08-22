ad4a1b89ddb601816ed63be94dc426ed75dd280e
Paul Simon Ongpin <paulsimon.ongpin@gmail.com>
2017-08-22 09:47:52 +0800

    Create Contact Section

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,8 +2,19 @@ import React from 'react'
 import { Container } from 'reactstrap'
 
 const Contact = () =>
-  <Container className="contact">
-    <h1>Contact</h1>
+  <Container className='contact text-center'>
+    <h2 className='section-header'>Interested working with me?</h2>
+    <a href='#' className='gf-link h5 btn'>Let's have a chat! </a>
+
+    <div className="social-links">
+      <h3>Find me on:</h3>
+      <p className="social-links-list">
+        <a href="#"><i className="fa fa-linkedin" aria-hidden="true"></i></a>
+        <a href="#"><i className="fa fa-github" aria-hidden="true"></i></a>
+        <a href="#"><i className="fa fa-facebook" aria-hidden="true"></i></a>
+        <a href="#"><i className="fa fa-twitter" aria-hidden="true"></i></a>
+      </p>
+    </div>
   </Container>
 
 export default Contact
--- src/layouts/index.js
+++ src/layouts/index.js
@@ -22,6 +22,7 @@ const TemplateWrapper = ({ children }) =>
       ]}
     >
       <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700" rel="stylesheet" />
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" />
     </Helmet>
 
     <div className='site-content'>
