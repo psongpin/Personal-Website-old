d296ac848eb8df2273146906a3fc2b28e0c9c81d
Paul Simon Ongpin <paulsimon.ongpin@gmail.com>
2017-09-04 23:36:23 +0800

    Add Portfolio section

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,9 +1,60 @@
 import React from 'react'
-import { Container } from 'reactstrap'
+import { Container, Row, Col } from 'reactstrap'
 
 const Portfolio = () =>
   <Container className="portfolio">
     <h2 className="section-header text-center">Portfolio</h2>
+
+    <Row className="portfolio-list">
+      <Col className="portfolio-list__item col-md-4">
+        <div className="card">
+          <img className="card-img-top img-fluid" src="http://via.placeholder.com/350x250" alt="test" />
+          <div className="card-block">
+            <p className="card-title"><a href="#">Card title</a></p>
+          </div>
+        </div>
+      </Col>
+      <Col className="portfolio-list__item col-md-4">
+        <div className="card">
+          <img className="card-img-top img-fluid" src="http://via.placeholder.com/350x250" alt="test" />
+          <div className="card-block">
+            <p className="card-title"><a href="#">Card title</a></p>
+          </div>
+        </div>
+      </Col>
+      <Col className="portfolio-list__item col-md-4">
+        <div className="card">
+          <img className="card-img-top img-fluid" src="http://via.placeholder.com/350x250" alt="test" />
+          <div className="card-block">
+            <p className="card-title"><a href="#">Card title</a></p>
+          </div>
+        </div>
+      </Col>
+      <Col className="portfolio-list__item col-md-4">
+        <div className="card">
+          <img className="card-img-top img-fluid" src="http://via.placeholder.com/350x250" alt="test" />
+          <div className="card-block">
+            <p className="card-title"><a href="#">Card title</a></p>
+          </div>
+        </div>
+      </Col>
+      <Col className="portfolio-list__item col-md-4">
+        <div className="card">
+          <img className="card-img-top img-fluid" src="http://via.placeholder.com/350x250" alt="test" />
+          <div className="card-block">
+            <p className="card-title"><a href="#">Card title</a></p>
+          </div>
+        </div>
+      </Col>
+      <Col className="portfolio-list__item col-md-4">
+        <div className="card">
+          <img className="card-img-top img-fluid" src="http://via.placeholder.com/350x250" alt="test" />
+          <div className="card-block">
+            <p className="card-title"><a href="#">Card title</a></p>
+          </div>
+        </div>
+      </Col>
+    </Row>
   </Container>
 
 export default Portfolio
