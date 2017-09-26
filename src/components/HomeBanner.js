2175fb1788e0b3e20a2a1fbeedc936b62cfdea22
Paul Simon Ongpin <paulsimon.ongpin@gmail.com>
2017-09-26 09:19:52 +0800

    Redesign Home Banner page

--- src/components/HomeBanner.js
+++ src/components/HomeBanner.js
@@ -1,19 +1,25 @@
 import React from 'react'
 import { Container, Row, Col } from 'reactstrap'
+import Link from 'gatsby-link'
+
+import divider from '../styles/Assets/Divider.svg'
+import paulImage from '../styles/Assets/paulImage.svg'
 
 const HomeBaner = () =>
   <Container className="home-banner">
     <Row className="home-banner-row">
 
-      <Col className="text-content home-banner-cols" xs="12" lg="6">
-        <h1>Paul Simon Ongpin</h1>
-        <hr className="divider" />
-        <p className="h4">Front-end Web Developer</p>
-        <p className="text">Committed to Learn Forever.</p>
+      <Col className="text-content home-banner-cols" xs="12" lg="4">
+        <div className="content-wrap">
+          <img src={divider} alt="" className="zigDivider"/>
+          <h1 className="h3">Hello I'm Paul.</h1>
+          <p>&lt; Front-end Web Developer /&gt;</p>
+          <Link to="/404" className="btn btn-rounded">Explore</Link>    
+        </div>    
       </Col>
       
-      <Col className="img-content home-banner-cols" xs="12" lg="6">
-        <img className="img-fluid" src="https://dummyimage.com/600x600/000/fff&text=+" alt="paul"/>
+      <Col className="img-content home-banner-cols" xs="12" lg="8">
+        <img className="img-fluid" src={paulImage} alt="paul"/>
       </Col>
     </Row>
   </Container>
